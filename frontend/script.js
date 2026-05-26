9bd2b7e402705e00c7ab6ec7880260ece0ee5e6a
sainiritika <ritikasaini.social@gmail.com>
2026-05-27 02:11:32 +0530

    updated script.js as netlify not working

--- frontend/script.js
+++ frontend/script.js
@@ -1,9 +1,8 @@
-//const ART_API = "http://localhost:8080/api/artworks";
-c//onst REVIEW_API = "http://localhost:8080/api/reviews";
 const ART_API = "https://art-gallery-project-java.onrender.com/api/artworks";
 const REVIEW_API = "https://art-gallery-project-java.onrender.com/api/reviews";
-async function fetchArtworks() {
 
+/* FETCH ARTWORKS */
+async function fetchArtworks() {
     const response = await fetch(ART_API);
     const data = await response.json();
 
@@ -11,7 +10,6 @@ async function fetchArtworks() {
     gallery.innerHTML = "";
 
     data.forEach(art => {
-
         gallery.innerHTML += `
             <div class="card">
                 <img src="${art.imageUrl}" />
@@ -23,8 +21,8 @@ async function fetchArtworks() {
     });
 }
 
+/* ADD ARTWORK */
 async function addArtwork() {
-
     const artwork = {
         title: document.getElementById("title").value,
         artist: document.getElementById("artist").value,
@@ -34,17 +32,15 @@ async function addArtwork() {
 
     await fetch(ART_API, {
         method: "POST",
-        headers: {
-            "Content-Type":"application/json"
-        },
+        headers: { "Content-Type": "application/json" },
         body: JSON.stringify(artwork)
     });
 
     fetchArtworks();
 }
 
+/* REVIEWS */
 async function fetchReviews() {
-
     const response = await fetch(REVIEW_API);
     const data = await response.json();
 
@@ -52,7 +48,6 @@ async function fetchReviews() {
     reviews.innerHTML = "";
 
     data.forEach(review => {
-
         reviews.innerHTML += `
             <div class="card">
                 <h3>${review.username}</h3>
@@ -63,7 +58,6 @@ async function fetchReviews() {
 }
 
 async function addReview() {
-
     const review = {
         username: document.getElementById("username").value,
         comment: document.getElementById("comment").value
@@ -71,14 +65,13 @@ async function addReview() {
 
     await fetch(REVIEW_API, {
         method: "POST",
-        headers: {
-            "Content-Type":"application/json"
-        },
+        headers: { "Content-Type": "application/json" },
         body: JSON.stringify(review)
     });
 
     fetchReviews();
 }
 
+/* INIT */
 fetchArtworks();
 fetchReviews();
