40b764a38213a559a78e7bcf21d5bb6f1d06d7e8
sainiritika <ritikasaini.social@gmail.com>
2026-05-27 02:33:45 +0530

    checking artwork entity

--- backend/src/main/java/com/artgallery/backend/model/Artwork.java
+++ backend/src/main/java/com/artgallery/backend/model/Artwork.java
@@ -6,7 +6,7 @@ import jakarta.persistence.*;
 public class Artwork {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @GeneratedValue
     private Long id;
 
     private String title;
